f351052d17eca8d4fcf081943298c4826eb3b2eb
Antonio Renaud <92221525+AntonioRenaud@users.noreply.github.com>
2023-02-03 12:33:32 -0400

    slightly modified the logic to marked the cells so the indexes of boardArray would match the counter

--- scripts.js
+++ scripts.js
@@ -4,20 +4,28 @@ const gameBoard = (() => {
     let container = document.querySelector('.container');
     let boardGrid = document.querySelector('.board-grid');
     const boardArray = [];
-    let i = 0;
+    let i = 1;
     
+    //Event listener to mark the cells, by adding the X or Circle class
     boardGrid.addEventListener('click', function(e){
+        // variable to capture the specific cell that was clicked upon
         let targetCell = e.target;
         if (!targetCell.classList.contains('marked') && targetCell.classList.contains('cell')){
+            //counter to help keep track of the turns, if i is a even number, it is X's turn
+            //if i is odd, it is circle's turn
             i++;
             if (i % 2 === 0 ) {
-                targetCell.classList.add('circle');
-                targetCell.classList.add('marked');
-                boardArray.push(`circle-${targetCell.dataset.cell}`);
-            } else {
                 targetCell.classList.add('x');
                 targetCell.classList.add('marked');
+                //pushes the specific mark and the position to the boardArray,
+                // with every index of the array being the turn
                 boardArray.push(`x-${targetCell.dataset.cell}`);
+                
+            } else {
+                targetCell.classList.add('circle');
+                targetCell.classList.add('marked');
+                
+                boardArray.push(`circle-${targetCell.dataset.cell}`);
                 }
           }
              
