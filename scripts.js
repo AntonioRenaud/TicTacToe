280e280dec90a6e16e0c59d44bcd812ba7568963
Antonio Renaud <92221525+AntonioRenaud@users.noreply.github.com>
2023-02-03 12:41:47 -0400

    Tweaked the logic of the evenlistener again, so the indexes of boardArray would match the i counter

--- scripts.js
+++ scripts.js
@@ -4,21 +4,21 @@ const gameBoard = (() => {
     let container = document.querySelector('.container');
     let boardGrid = document.querySelector('.board-grid');
     const boardArray = [];
-    let i = 1;
+    let i = 0;
     
     //Event listener to mark the cells, by adding the X or Circle class
     boardGrid.addEventListener('click', function(e){
         // variable to capture the specific cell that was clicked upon
         let targetCell = e.target;
+        // Will only add a class if it wasn't marked previously and is a cell
         if (!targetCell.classList.contains('marked') && targetCell.classList.contains('cell')){
-            //counter to help keep track of the turns, if i is a even number, it is X's turn
-            //if i is odd, it is circle's turn
-            i++;
+            
             if (i % 2 === 0 ) {
                 targetCell.classList.add('x');
                 targetCell.classList.add('marked');
                 //pushes the specific mark and the position to the boardArray,
-                // with every index of the array being the turn
+                // with every index of the array being the turn and the position
+                // taken from data-cell parameter of the cell
                 boardArray.push(`x-${targetCell.dataset.cell}`);
                 
             } else {
@@ -27,11 +27,16 @@ const gameBoard = (() => {
                 
                 boardArray.push(`circle-${targetCell.dataset.cell}`);
                 }
+            //counter to help keep track of the turns, if i is a even number, it is X's turn
+            //if i is odd, it is circle's turn
+            i++;
           }
+          
              
         });
     return {
         boardArray,
+        
     };
 })();
 //fill the cells
