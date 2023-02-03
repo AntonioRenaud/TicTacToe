6099c4ee666e8685471986db699872fc003a19cd
Antonio Renaud <92221525+AntonioRenaud@users.noreply.github.com>
2023-02-02 23:36:56 -0400

    Added initial logic for the gameboard and some styles

---
+++ scripts.js
@@ -0,0 +1,45 @@
+//module for the gameboard
+
+const gameBoard = (() => {
+    let container = document.querySelector('.container');
+    let boardGrid = document.querySelector('.board-grid');
+    const boardArray = [];
+    let i = 0;
+    
+    boardGrid.addEventListener('click', function(e){
+        let targetCell = e.target;
+        if (!targetCell.classList.contains('marked') && targetCell.classList.contains('cell')){
+            i++;
+            if (i % 2 === 0 ) {
+                targetCell.classList.add('circle');
+                targetCell.classList.add('marked');
+                boardArray.push('circle');
+            }
+            else {
+                targetCell.classList.add('x');
+                targetCell.classList.add('marked');
+                boardArray.push('x');
+            }
+        }
+        
+    
+            
+        });
+   
+   
+
+    
+  
+   
+
+    return {
+        boardArray,
+    };
+})();
+//fill the cells
+
+//create players
+
+//check winner
+
+//keep score
